c489cc32fd8e5e08d6eb010f840b03f90fe017c4
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-10-30 22:28:26 +0100

    added locationblogfacade metoderne findlocationblog, likelocationblog, og addlocationblog og lavet passed tests på dem

--- facades/blogFacade.js
+++ facades/blogFacade.js
@@ -3,37 +3,36 @@ require("../dbSetup.js")();
 var users = require('../models/User.js');
 var User = mongoose.model("User", users.userSchema);
 var locblogs = require('../models/LocationBlog.js');
-var LocBlog= mongoose.model("LocationBlog", locblogs.locationBlogSchema);
+var LocBlog = mongoose.model("LocationBlog", locblogs.locationBlogSchema);
 
-
-function addLocationBlog (info,longitude, lattitude, author){
-  
-
-
-  
-    let position= {longitude:longitude, latitude:lattitude};
-let locblogDetail= {info:info,pos:position, author:author, likedBy:author._id};
-var blog = new LocBlog(locblogDetail);
-
-return blog.save();
-}
-async function likeLocationBlog(blogid, likedbyid){
-
-   const likedList=[];
-  
-  await LocBlog.findOne({ _id: blogid}).select('likedBy').exec(function (err,res){
-  
-    likedList.push(likedbyid)
-  res.likedBy.forEach(element => {
-    likedList.push(element);
-  });  LocBlog.updateOne({ _id: blogid }, { $set: { likedBy: likedList }}).exec();
-   
-  });
-
-  
-
-
-    
+async function getAllBlogs() {
+  return await LocBlog.find({}).exec();
 }
 
-module.exports={addLocationBlog:addLocationBlog,likeLocationBlog:likeLocationBlog}
+function addLocationBlog(info, longitude, lattitude, author) {
+
+  let position = { longitude: longitude, latitude: lattitude };
+  let locblogDetail = { info: info, pos: position, author: author, likedBy: author._id };
+  var blog = new LocBlog(locblogDetail);
+
+  return blog.save();
+}
+
+async function findLocationBlog(id) {
+  return await LocBlog.findById({ _id: id }).exec();
+}
+async function likeLocationBlog(id, user_id) {
+  var blog = await LocBlog.findOneAndUpdate({ _id: id }, { $push: { likedBy: user_id } });
+  return blog.save();
+}
+
+
+
+
+
+module.exports = {
+  addLocationBlog: addLocationBlog,
+  likeLocationBlog: likeLocationBlog,
+  getAllBlogs: getAllBlogs,
+  findLocationBlog: findLocationBlog
+}
