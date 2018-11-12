fb025bd0750a3efa93ea1f6182b0f58c7d4fb62b
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-11-12 23:32:20 +0100

    changed in login endpoint error related

--- routes/api.js
+++ routes/api.js
@@ -168,12 +168,12 @@ router.post('/login', async function (req, res, next) {
   try {
     let loginUser = await loginFacade.login(req.body.userName, req.body.password, req.body.longitude, req.body.latitude, req.body.distance);
     let responseObk = res.json(loginUser)
-
+console.log(responseObk)
     // If user or password does not exist send a messege to the client.
     if (res.status(404)) {
       res.json({
         status:404,
-        message:  res.message
+        message:  responseObk
       });
     } else {
       res.render('login', {
